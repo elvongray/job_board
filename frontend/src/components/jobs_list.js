92e66b26a31fd2f066ff93d82d1eeeb2ad1a29b9
Eniola Arinde <eniolaarinde1@gmail.com>
2021-01-21 12:14:18 +0100

    * Complete the basic functionalities for the website * Some bug fixes and cleanup

--- frontend/src/components/jobs_list.js
+++ frontend/src/components/jobs_list.js
@@ -17,75 +17,98 @@ function formatDateTime(time) {
   return duration.humanize();
 }
 
-
 function JobsList({
-    fetchingFailed, isFetechingJobs, currentJob, jobs, currentPage, selectCurrentJob, fetchPage
-  }) {
+  fetchingFailed,
+  isFetechingJobs,
+  currentJob,
+  jobs,
+  currentPage,
+  selectCurrentJob,
+  fetchPage,
+}) {
   return (
-    <Grid container classes={{root: 'jobs-list-container'}}>
-      {fetchingFailed &&
-        <Grid item xs={12} classes={{root: 'failed'}}>
-            <div>No jobs found</div>
+    <Grid container classes={{ root: 'jobs-list-container' }}>
+      {fetchingFailed && (
+        <Grid item xs={12} classes={{ root: 'failed' }}>
+          <div>No jobs found</div>
         </Grid>
-      }
-      {isFetechingJobs && !fetchingFailed &&
-        <Grid item xs={12} classes={{root: 'progress-container'}}>
-            <div className="loader"></div>
+      )}
+      {isFetechingJobs && !fetchingFailed && (
+        <Grid item xs={12} classes={{ root: 'progress-container' }}>
+          <div className="loader"></div>
         </Grid>
-      }
-      {!isFetechingJobs && !fetchingFailed &&
-      <React.Fragment>
-        <Grid item xs={5} classes={{root: 'jobs-list'}}>
-          <List>
+      )}
+      {!isFetechingJobs && !fetchingFailed && (
+        <React.Fragment>
+          <Grid item xs={5} classes={{ root: 'jobs-list' }}>
+            <List>
               {jobs.map((job) => {
-                  const rootClass = job.id === currentJob.id ? 'current-job' : '';
+                const rootClass = job.id === currentJob.id ? 'current-job' : '';
 
-                  return (
-                  <ListItem classes={{root: `job-list-item ${rootClass}`}} key={job.id}
-                              onClick={() => selectCurrentJob(job)}>
-                      <div className="job-cont">
-                        <div className="job-title">{ job.title }</div>
-                        <div className="company">{ job.company }</div>
+                return (
+                  <ListItem
+                    classes={{ root: `job-list-item ${rootClass}` }}
+                    key={job.id}
+                    onClick={() => selectCurrentJob(job)}
+                  >
+                    <div className="job-cont">
+                      <div className="job-title">{job.title}</div>
+                      <div className="company">{job.company}</div>
+                    </div>
+                    <div className="location-cont">
+                      <div className="location">{job.location}</div>
+                      <div className="time">
+                        {formatDateTime(job.created_at)}
                       </div>
-                      <div className="location-cont">
-                        <div className="location">{ job.location }</div>
-                        <div className="time">{formatDateTime(job.created_at)}</div>
-                      </div>
+                    </div>
                   </ListItem>
-                  )
-              })
-              }
-          </List>
-        </Grid>
-        <Grid item xs={7} classes={{root: 'job-description'}}>
-          <div className="job-header">
-              {currentJob.company_logo &&
-                <img src={currentJob.company_logo} key={currentJob.company_logo} alt=""/>
-              }
+                );
+              })}
+            </List>
+          </Grid>
+          <Grid item xs={7} classes={{ root: 'job-description' }}>
+            <div className="job-header">
+              {currentJob.company_logo && (
+                <img
+                  src={currentJob.company_logo}
+                  key={currentJob.company_logo}
+                  alt=""
+                />
+              )}
               <h1>{currentJob.title}</h1>
-              <a href={currentJob.company_url} rel="noreferrer" target="_blank">{ currentJob.company }</a>
-          </div>
-          <h2>Job description</h2>
-          <div dangerouslySetInnerHTML={{__html : currentJob.description}} />
-          <h2>How to apply</h2>
-          <div dangerouslySetInnerHTML={{__html : currentJob.how_to_apply}} />
-        </Grid>
-      </React.Fragment>
-      }
-      <Grid xs={12} classes={{root: 'pagination'}}>
-        <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
-            <Button
-              startIcon={<ArrowBackIcon />}
-              onClick={() => fetchPage('back')}
-              disabled={currentPage === 0}>
-              Back
-            </Button>
-            <Button
-              endIcon={<ArrowForwardIcon />}
-              onClick={() => fetchPage('next')}
-              disabled={currentPage === 4}>
-              Next
-            </Button>
+              <a href={currentJob.company_url} rel="noreferrer" target="_blank">
+                {currentJob.company}
+              </a>
+            </div>
+            <h2>Job description</h2>
+            <div dangerouslySetInnerHTML={{ __html: currentJob.description }} />
+            <h2>How to apply</h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: currentJob.how_to_apply }}
+            />
+          </Grid>
+        </React.Fragment>
+      )}
+      <Grid xs={12} classes={{ root: 'pagination' }}>
+        <ButtonGroup
+          size="large"
+          color="primary"
+          aria-label="large outlined primary button group"
+        >
+          <Button
+            startIcon={<ArrowBackIcon />}
+            onClick={() => fetchPage('back')}
+            disabled={currentPage === 0}
+          >
+            Back
+          </Button>
+          <Button
+            endIcon={<ArrowForwardIcon />}
+            onClick={() => fetchPage('next')}
+            disabled={currentPage === 4}
+          >
+            Next
+          </Button>
         </ButtonGroup>
       </Grid>
     </Grid>
@@ -94,12 +117,12 @@ function JobsList({
 
 JobsList.propTypes = {
   fetchingFailed: PropTypes.bool.isRequired,
-  isFetechingJobs : PropTypes.bool.isRequired,
+  isFetechingJobs: PropTypes.bool.isRequired,
   currentJob: PropTypes.object.isRequired,
   jobs: PropTypes.array.isRequired,
   currentPage: PropTypes.number.isRequired,
   selectCurrentJob: PropTypes.func.isRequired,
   fetchPage: PropTypes.func.isRequired,
-}
+};
 
-export default JobsList
+export default JobsList;
--- frontend/src/job_board.js
+++ frontend/src/job_board.js
@@ -13,25 +13,24 @@ import InputLabel from '@material-ui/core/InputLabel';
 
 import JobsList from './components/jobs_list';
 
-
 import './job_board.scss';
 
 const LOCATIONS = {
-  'chicago': 'Chicago',
+  chicago: 'Chicago',
   'san+francisco': 'San Francisco',
-  'phoenix': 'Phoenix',
-  'london': 'London',
-  'beijing': 'Beijing',
-  'paris': 'Paris',
+  phoenix: 'Phoenix',
+  london: 'London',
+  beijing: 'Beijing',
+  paris: 'Paris',
 };
 
 const LANGUAGE = {
-  'javascript': 'Javascript',
-  'java': 'Java',
-  'python': 'Python',
-  'react': 'React',
-  'ruby': 'Ruby',
-  'go': 'Go',
+  javascript: 'Javascript',
+  java: 'Java',
+  python: 'Python',
+  react: 'React',
+  ruby: 'Ruby',
+  go: 'Go',
 };
 
 class JobBoard extends Component {
@@ -47,105 +46,141 @@ class JobBoard extends Component {
       location: '',
       language: '',
       search: '',
-    }
+    };
   }
 
   componentDidMount() {
-    this.setState({
-      fetchingFailed: false,
-      isFetechingJobs: true,
-    }, () => {
-      axios.get('/positions')
-        .then((response) => {
-          this.setState({
-            jobs: response.data,
-            isFetechingJobs: false,
-            currentJob: response.data[0],
+    // Fetch the first page of jobs
+    this.setState(
+      {
+        fetchingFailed: false,
+        isFetechingJobs: true,
+      },
+      () => {
+        axios
+          .get('/positions')
+          .then((response) => {
+            this.setState({
+              jobs: response.data,
+              isFetechingJobs: false,
+              currentJob: response.data[0],
+            });
+          })
+          .catch((error) => {
+            this.setState({ fetchingFailed: true });
           });
-        })
-        .catch((error) =>  {
-          this.setState({ fetchingFailed: true });
-          console.log('crap', error);
-        });
-    })
+      }
+    );
   }
 
   selectCurrentJob = (currentJob) => {
     this.setState({ currentJob });
-  }
+  };
 
+  // Fetch jobs using the user selected location and description
   searchForJob = (e) => {
     if (e && e.keyCode !== 13) return;
-    const { currentPage, location,  language,  search } = this.state
+    const { location, language, search } = this.state;
 
-    this.setState({
-      fetchingFailed: false,
-      isFetechingJobs: true,
-    }, () => {
-      axios.get('/positions', {
-        params: {
-          search,
-          page: currentPage,
-          location,
-          language,
-        }
-      }).then((response) => {
-          this.setState({
-            jobs: response.data,
-            isFetechingJobs: false,
-            currentJob: response.data[0],
+    this.setState(
+      {
+        fetchingFailed: false,
+        isFetechingJobs: true,
+      },
+      () => {
+        axios
+          .get('/positions', {
+            params: {
+              search,
+              page: 0,
+              location,
+              language,
+            },
+          })
+          .then((response) => {
+            this.setState({
+              jobs: response.data,
+              isFetechingJobs: false,
+              currentJob: response.data[0],
+            });
+          })
+          .catch((error) => {
+            this.setState({ fetchingFailed: true });
+            console.log('crap', error);
           });
-        })
-        .catch((error) =>  {
-          this.setState({ fetchingFailed: true });
-          console.log('crap', error);
-        });
-    })
-  }
+      }
+    );
+  };
 
   fetchPage = (pageDirection) => {
-    const { currentPage } = this.state;
+    const { currentPage, location, language, search } = this.state;
     let page = null;
-    page = pageDirection === 'back' ? Math.max(0, currentPage - 1) : Math.min(4, currentPage + 1)
+    let extraParams = {};
 
-    this.setState({
-      fetchingFailed: false,
-      isFetechingJobs: true,
-    }, () => {
-      axios.get('/positions', {
-        params: { page }
-      }).then((response) => {
-          this.setState({
-            currentPage: page,
-            jobs: response.data,
-            isFetechingJobs: false,
-            currentJob: response.data[0],
+    page =
+      pageDirection === 'back'
+        ? Math.max(0, currentPage - 1)
+        : Math.min(4, currentPage + 1);
+
+    if (location || language || search) {
+      extraParams = { location, language, search };
+    }
+
+    this.setState(
+      {
+        fetchingFailed: false,
+        isFetechingJobs: true,
+      },
+      () => {
+        axios
+          .get('/positions', {
+            params: {
+              page,
+              ...extraParams,
+            },
+          })
+          .then((response) => {
+            this.setState({
+              currentPage: page,
+              jobs: response.data,
+              isFetechingJobs: false,
+              currentJob: response.data[0],
+            });
+          })
+          .catch((error) => {
+            this.setState({ fetchingFailed: true });
+            console.log('crap', error);
           });
-        })
-        .catch((error) =>  {
-          this.setState({ fetchingFailed: true });
-          console.log('crap', error);
-        });
-    })
-  }
+      }
+    );
+  };
 
   render() {
-    const { jobs, currentJob, currentPage, isFetechingJobs,
-            location, language, fetchingFailed } = this.state;
+    const {
+      jobs,
+      currentJob,
+      currentPage,
+      isFetechingJobs,
+      location,
+      language,
+      fetchingFailed,
+    } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <h1>Job Search</h1>
-          <p>Find your next career at some of the best companies in the world</p>
+          <p>
+            Find your next career at some of the best companies in the world
+          </p>
         </header>
         <Container maxWidth="lg">
           <Grid container>
-            <Grid item xs={12} classes={{root: 'input-container'}}>
+            <Grid item xs={12} classes={{ root: 'input-container' }}>
               <TextField
                 id="standard-basic"
                 label="Enter job title or keyword and use the filters below to get best results"
-                onChange={(e) => this.setState({search : e.target.value})}
+                onChange={(e) => this.setState({ search: e.target.value })}
                 onKeyDown={(e) => this.searchForJob(e)}
                 InputProps={{
                   startAdornment: (
@@ -158,39 +193,42 @@ class JobBoard extends Component {
               <Button
                 variant="contained"
                 color="primary"
-                onClick={() => this.searchForJob()}>
+                onClick={() => this.searchForJob()}
+              >
                 Search
               </Button>
             </Grid>
-            <Grid item xs={12} classes={{root: 'select-container'}}>
-              <FormControl classes={{root: 'city-select'}}>
+            <Grid item xs={12} classes={{ root: 'select-container' }}>
+              <FormControl classes={{ root: 'city-select' }}>
                 <InputLabel id="city-select-label">Select city</InputLabel>
                 <Select
                   labelId="city-select-label"
                   id="city-select-label"
                   value={location}
-                  onChange={(e) => this.setState({location: e.target.value})}
+                  onChange={(e) => this.setState({ location: e.target.value })}
                 >
                   <MenuItem value="">None</MenuItem>
                   {Object.entries(LOCATIONS).map(([key, value]) => (
-                      <MenuItem key={key} value={key}>{value}</MenuItem>
-                    ))
-                  }
+                    <MenuItem key={key} value={key}>
+                      {value}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-              <FormControl classes={{root: 'job-select'}}>
+              <FormControl classes={{ root: 'job-select' }}>
                 <InputLabel id="job-select-label">Select job type</InputLabel>
                 <Select
                   labelId="job-select-label"
                   id="job-select-label"
                   value={language}
-                  onChange={(e) => this.setState({language: e.target.value})}
+                  onChange={(e) => this.setState({ language: e.target.value })}
                 >
                   <MenuItem value="">None</MenuItem>
                   {Object.entries(LANGUAGE).map(([key, value]) => (
-                      <MenuItem key={key} value={key}>{value}</MenuItem>
-                    ))
-                  }
+                    <MenuItem key={key} value={key}>
+                      {value}
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
             </Grid>
